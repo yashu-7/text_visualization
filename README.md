c36cdf94e2f46a6e58b5b255f1c6ccb0929b25c3
yashu-7 <yaswanthreddypalle@gmail.com>
2024-07-18 21:43:12 +0530

    Done with words and occurances

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-# text_visualization
+# Text Visualization
+
+### How the package works
+* Takes in path to file from cli
+* Magic pot is a story taken from google
+* Example
+```
+cargo run MagicPot.txt
+```
+* Preprocessing
+    1. Removes punctuations like ",.\!?"
+    2. Converts all the words to lower case
+* Puts the words and their occurances into a Hashmap
+
+#### Future updates
+* To visualize it on a graph
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,35 @@
+use std::collections::HashMap;
+use regex::Regex;
+use std::env;
+use std::fs;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<_> = env::args().collect();
+    println!("{:?}", args);
+
+    if args.len() < 2 {
+        println!("Usage: <file-path>");
+        return;
+    }
+
+    let file_path = &args[1];
+    // println!("{}", file_path);
+
+    let content = fs::read_to_string(file_path).expect("Unable to read from file");
+
+    // Preprocessing to remove punctuations and converting to lowercase
+    let re = Regex::new(r"[,.?!']").unwrap();
+    let clean_text = re.replace_all(&content, "").to_lowercase();
+
+    let mut words_count: HashMap<String, u32> = HashMap::new();
+
+    let words = clean_text.split_whitespace();
+    for word in words {
+        let count = words_count.entry(word.to_string()).or_insert(0);
+        *count += 1;
+    }
+
+    for (word, count) in words_count.iter() {
+        println!("{}\t--->\t{}",word,count);
+    }
 }
